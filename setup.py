a641828384eed30353a3b1e16ad6f757b6661977
Guy Sheffer <guysoft@gmail.com>
2025-11-23 13:27:52 +0200

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ script_name = "bin/pip.exe" if sys.platform == "win32" else "bin/pip"
 
 setup(
     name="pip-uv",
-    version="0.1.4",
+    version="0.1.5",
     # We declare the compiled binary as a "script" so pip installs it to bin/
     scripts=[script_name],
     distclass=BinaryDistribution,
